f690ec39d7a61ba6711e5a6813c2648a25c3617d
BuildTools <unconfigured@null.spigotmc.org>
2017-10-20 15:36:10 +0200

    Updated dice roll module.

--- modules/dice_roll.py
+++ modules/dice_roll.py
@@ -12,7 +12,10 @@ def main(update):
         split = string.split()
         if split[1].isdigit():
             sides = int(split[1])
-            rolled = random.randint(1, sides)
-            return sloth.sendmessage(update.get('channel'), str(rolled))
+            if sides >= 2:
+                rolled = random.randint(1, sides)
+                return sloth.sendmessage(update.get('channel'), str(rolled))
+            else:
+                print('That is impossible in this reality.')
         else:
             return sloth.sendmessage(update.get('channel'), 'Please input a number.')
