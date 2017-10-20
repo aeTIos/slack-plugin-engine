a659de3b15616647d17c3d7c71ed7946e68b1072
Richard van Dijk <dijkvanrichard@gmail.com>
2017-10-20 12:06:54 +0200

    style

--- bot.py
+++ bot.py
@@ -7,21 +7,21 @@ import sloth
 
 import module_import
 
-#init variables to handle messaging and input of the API key
+# init variables to handle messaging and input of the API key
 slack_api_key = sys.argv[1]
 slack = Slacker(slack_api_key)
 weburl = slack.rtm.start().body['url']
 
-#creates websocket
+# creates websocket connection
 socket = websocket.create_connection(weburl, sslopt={"cert_reqs": ssl.CERT_NONE})
 
-#creates module array from python files in the modules directory
+# creates module array from python files in the modules directory
 module_array, module_names = module_import.loadmodules('modules/')
 
 for i in range(len(module_array)):  # temporary code. In the future, we should read this from a config file.
     module_array[i] = [module_array[i], True]
 
-#Main loop
+# Main loop
 while True:
     update = socket.recv()
     update = json.JSONDecoder().decode(update)
@@ -41,5 +41,5 @@ while True:
                 # print('Error:', e)
                 # return error and log
                 socket.send(sloth.sendmessage(update.get('channel'),
-                                            f"We caught an error and the module has been disabled.\nError: {e}"))
+                                              f"We caught an error and the module has been disabled.\nError: {e}"))
                 i[1] = False  # set module to OFF so we don't get further errors
--- sloth.py
+++ sloth.py
@@ -8,26 +8,28 @@ def texthandler(handlerfunction):
 
     return wrapper
 
+
 # sends channel message
-#input channel name and text to send
-#return Json encoded payload to socks
+# input channel name and text to send
+# return Json encoded payload to socks
 def sendmessage(channel, text):
     payload = json.JSONEncoder().encode({"id": 1, "type": "message", "channel": channel, "text": text})
     return payload
 
+
 # sends private message
-#input user name and text to send
-#return Json encoded payload
-#TODO: test function to see if it works
+# input user name and text to send
+# return Json encoded payload
+# TODO: test function to see if it works
 def sendprivmessage(user,text):
     payload = json.JSONEncoder().encode({"id": 1, " type": "message", "channel": user, "text": text})
     return payload
 
 
-#uploads image
-#TODO: STUB
+# uploads image
+# TODO: STUB
 def uploadimage():
-
+    pass
 
 
 
